e99c45218d8c9c77ad31f3b4289b5ba59b473fe4
Federico Iza <fedegiza@gmail.com>
2024-09-07 11:59:50 -0300

    Add documentation.

--- Common/Coordinates.cs
+++ Common/Coordinates.cs
@@ -3,17 +3,30 @@ using System.Numerics;
 
 namespace RayTracingRenderer.Common
 {
-    public class Coordinates
+    public static class Coordinates
     {
-        public Coordinates() { }
-
+        /// <summary>
+        /// Return the viewport space coordinates based on the canvas space coordinates.
+        /// </summary>
+        /// <param name="xCanvas">The x position in canvas coordinates.</param>
+        /// <param name="yCanvas">The y position in canvas coordinates.</param>
+        /// <param name="canvas">The <c>Canvas</c> instance.</param>
+        /// <param name="viewport">The <c>Viewport</c> instance.</param>
+        /// <returns>The coordinates in viewport space.</returns>
         public static Vector2 CanvasToViewport(int xCanvas, int yCanvas, Canvas canvas, Viewport viewport)
         {
-            float xViewport = (float) xCanvas * viewport.GetWidth() / canvas.GetWidth();
-            float yViewport = (float) yCanvas * viewport.GetHeight() / canvas.GetHeight();
+            float xViewport = xCanvas * viewport.GetWidth() / canvas.GetWidth();
+            float yViewport = yCanvas * viewport.GetHeight() / canvas.GetHeight();
             return new Vector2(xViewport, yViewport);
         }
 
+        /// <summary>
+        /// Return the screen space coordinates based on the canvas space coordinates.
+        /// </summary>
+        /// <param name="xCanvas">The x position in canvas coordinates.</param>
+        /// <param name="yCanvas">The y position in canvas coordinates.</param>
+        /// <param name="canvas">The <c>Canvas</c> instance.</param>
+        /// <returns>The coordinates in screen space.</returns>
         public static Vector2 CanvasToScreen(int xCanvas, int yCanvas, Canvas canvas)
         {
             int xScreen = xCanvas + canvas.GetWidth() / 2;
@@ -21,6 +34,14 @@ namespace RayTracingRenderer.Common
             return new Vector2(xScreen, yScreen);
         }
 
+
+        /// <summary>
+        /// Return the canvas space coordinates based on the screen space coordinates.
+        /// </summary>
+        /// <param name="xScreen">The x position in screen coordinates.</param>
+        /// <param name="yScreen">The y position in screen coordinates.</param>
+        /// <param name="canvas">The <c>Canvas</c> instance.</param>
+        /// <returns>The coordinates in canvas space.</returns>
         public static Vector2 ScreenToCanvas(int xScreen, int yScreen, Canvas canvas)
         {
             int xCanvas = xScreen - canvas.GetWidth() / 2;
